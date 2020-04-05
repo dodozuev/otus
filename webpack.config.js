7eba50e21d47ff8579959d0b5453d74ec9a1b5c8
d.zuev <d.zuev@dodopizza.com>
2020-04-05 13:09:51 +0300

    Finish setting up eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
-const webpack = require("webpack"); //to access built-in plugins
 const path = require("path");
 
 module.exports = {
