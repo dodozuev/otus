3f4ce953f8e8ee01222ad559513765cacb06e85d
d.zuev <d.zuev@dodopizza.com>
2020-04-25 16:53:31 +0300

    bring back npm

--- src/lesson4/components/Wallet/wallet.tsx
+++ src/lesson4/components/Wallet/wallet.tsx
@@ -1,24 +1,43 @@
-import { AvailableCoinValues, Coin } from "./components/WalletItem/coin";
+import React, { Component } from "react";
 
-import React from "react";
+import { Coin } from "./components/WalletItem/coin";
 import { WalletStyled } from "./wallet.styles";
 
 interface WalletProps {
   ownerName: string;
   coins: number[];
 }
-export const Wallet = (props: WalletProps) => {
-  var badCoins = props.coins.filter(
-    (x) => !AvailableCoinValues.some((y) => y === x)
-  );
-  if (badCoins.length > 0)
-    return <div>{"Error: unable to create coin from " + badCoins}</div>;
-  return (
-    <WalletStyled>
-      <span>{props.ownerName + "'s wallet"}</span>
-      {props.coins.map((c, index) => (
-        <Coin key={index} value={c} />
-      ))}
-    </WalletStyled>
-  );
-};
+
+interface WalletState {
+  hasError: boolean;
+}
+const initWallet = (): WalletState => ({ hasError: false });
+
+export class Wallet extends Component<WalletProps, WalletState> {
+  constructor(props) {
+    super(props);
+    this.state = initWallet();
+  }
+
+  componentDidMount() {
+    this.setState({ ...this.state, hasError: false });
+  }
+
+  componentDidCatch(error) {
+    console.log("error:", error);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    return this.state.hasError ? (
+      <div>{"Произошла ошибка :'("}</div>
+    ) : (
+      <WalletStyled>
+        <span>{this.props.ownerName + "'s wallet"}</span>
+        {this.props.coins.map((c, index) => (
+          <Coin key={index} value={c} />
+        ))}
+      </WalletStyled>
+    );
+  }
+}
