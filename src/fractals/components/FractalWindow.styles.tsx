582f6058b76b53bfbfc44e9eb291f9f92a33128a
d.zuev <d.zuev@dodopizza.com>
2020-05-03 09:40:16 +0300

    add FractalWindow

---
+++ src/fractals/components/FractalWindow.styles.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const FractalContainer = styled.div``;
+
+export const FractalField = styled.div`
+  width: 500px;
+  height: 500px;
+`;
--- src/fractals/components/FractalWindow.tsx
+++ src/fractals/components/FractalWindow.tsx
@@ -1,10 +1,13 @@
+import { FractalContainer, FractalField } from "./FractalWindow.styles";
+
 import { Menu } from "./Menu/Menu";
 import React from "react";
 
 export const FractalWindow = () => {
   return (
-    <>
+    <FractalContainer>
       <Menu onSubmit={() => {}} />
-    </>
+      <FractalField />
+    </FractalContainer>
   );
 };
